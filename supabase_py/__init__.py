30b486a1915d04e8092ba4bcacd759c37e4f7297
leonfedden <leonfedden@gmail.com>
2021-02-05 10:56:42 +0000

    add version to package

--- supabase_py/__init__.py
+++ supabase_py/__init__.py
@@ -1,2 +1,5 @@
-from .lib import *
-from .client import Client
+from lib import supabase_auth_client, supabase_query_builder, supabase_realtime_client
+from client import Client
+
+
+__VERSION__ = "0.0.1"
