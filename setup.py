6c9e99c5e23f3a30715405acc828c362e36672ec
leonfedden <leonfedden@gmail.com>
2021-02-05 23:49:09 +0000

    add shim

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-import supabase_py
+#!/usr/bin/env python
 
 import setuptools
 
-
-def get_package_description() -> str:
-    """Returns a description of this package from the markdown files."""
-    with open("README.md", "r") as stream:
-        readme: str = stream.read()
-    return readme
-
-
-setuptools.setup(
-    name="mitto",
-    version=supabase_py.__version__,
-    author="Joel Lee, Leon Fedden",
-    author_email="joel@joellee.org",
-    description="Supabase client for Python.",
-    long_description=get_package_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/supabase/supabase-py",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+if __name__ == "__main__":
+    setuptools.setup()
