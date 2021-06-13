41682adee5a7ec93c8382cf376cd4729c9360ffa
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-06-13 22:21:01 +0800

    feat: Add more functions to storage file api

--- supabase_py/lib/storage/storage_file_api.py
+++ supabase_py/lib/storage/storage_file_api.py
@@ -3,6 +3,15 @@ from requests import HTTPError
 
 
 class StorageFileApi:
+    DEFAULT_SEARCH_OPTIONS = {
+        "limit": 100,
+        "offset": 0,
+        "sortBy": {
+            "column": "name",
+            "order": "asc",
+        },
+    }
+
     def __init__(self, url: str, headers: dict, bucket_id: str):
         """
         Parameters
@@ -50,7 +59,79 @@ class StorageFileApi:
             return data
 
     def move(self, from_path: str, to_path: str):
-        pass
+        """
+        Moves an existing file, optionally renaming it at the same time.
+        Parameters
+        ----------
+        from_path
+            The original file path, including the current file name. For example `folder/image.png`.
+        to_path
+            The new file path, including the new file name. For example `folder/image-copy.png`.
+        """
+        try:
+            response = requests.post(
+                f"{self.url}/object/move",
+                data={
+                    "bucketId": self.bucket_id,
+                    "sourceKey": from_path,
+                    "destinationKey": to_path,
+                },
+                headers=self.headers,
+            )
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")  # Python 3.6
+        except Exception as err:
+            print(f"Other error occurred: {err}")  # Python 3.6
+        else:
+            return response.json()
+
+    def remove(self, paths: list):
+        """
+        Deletes files within the same bucket
+        Parameters
+        ----------
+        paths
+            An array or list of files to be deletes, including the path and file name. For example [`folder/image.png`].
+        """
+        try:
+            response = requests.delete(
+                f"{self.url}/object/{self.bucket_id}",
+                data={"prefixes": paths},
+                headers=self.headers,
+            )
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")  # Python 3.6
+        except Exception as err:
+            raise err  # Python 3.6
+        else:
+            return response.json()
+
+    def list(self, path: str = None, options: dict = {}):
+        """
+        Lists all the files within a bucket.
+        Parameters
+        ----------
+        path
+            The folder path.
+        options
+            Search options, including `limit`, `offset`, and `sortBy`.
+        """
+        try:
+            body = dict(self.DEFAULT_SEARCH_OPTIONS, **options)
+            headers = dict(self.headers, **{"Content-Type": "application/json"})
+            body["prefix"] = path if path else ""
+            getdata = requests.post(
+                f"{self.url}/object/list/{self.bucket_id}", json=body, headers=headers
+            )
+            getdata.raise_for_status()
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")  # Python 3.6
+        except Exception as err:
+            raise err  # Python 3.6
+        else:
+            return getdata.json()
 
     def _get_final_path(self, path: str):
         return f"{self.bucket_id}/{path}"
