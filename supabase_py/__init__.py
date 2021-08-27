027bfb5657acfb97c24f64d729b6cd0321ac2547
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2021-08-27 23:51:24 +0700

    Fix circular imports

--- supabase_py/__init__.py
+++ supabase_py/__init__.py
@@ -1,6 +1,6 @@
+__version__ = "0.0.2"
+
 from supabase_py import client, lib
 from supabase_py.client import Client, create_client
-
-__version__ = "0.0.2"
 
 __all__ = ["client", "lib", "Client", "create_client"]
