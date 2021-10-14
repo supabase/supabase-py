0ba494cf62c2923b3903cee1651be8abebb454d1
Juliano Fernandes <julianofernandes@gmail.com>
2021-10-14 01:30:30 -0300

    build: add requests-toolbelt to the dependencies list
    feat: add mime type to uploaded files
    
    test: ensure upload files works properly

--- tests/test_client.py
+++ tests/test_client.py
@@ -97,3 +97,33 @@ def test_client_bucket(supabase: Client) -> None:
     # storage_bucket.get_bucket(TEST_BUCKET_NAME)
     # storage_bucket.empty_bucket(TEST_BUCKET_NAME)
     # storage_bucket.delete_bucket(TEST_BUCKET_NAME)
+
+
+@pytest.mark.skip(reason="missing permissions on test instance")
+def test_client_upload_file(supabase: Client) -> None:
+    """Ensure we can upload files to a bucket"""
+
+    TEST_BUCKET_NAME = "atestbucket"
+
+    storage = supabase.storage()
+    storage_file = storage.StorageFileAPI(TEST_BUCKET_NAME)
+
+    filename = "test.jpeg"
+    filepath = f"tests/{filename}"
+    mimetype = "image/jpeg"
+    options = {"contentType": mimetype}
+
+    storage_file.upload(filename, filepath, options)
+    files = storage_file.list()
+    assert len(files) > 0
+
+    image_info = None
+    for item in files:
+        if item.get("name") == filename:
+            image_info = item
+            break
+
+    assert image_info is not None
+    assert image_info.get("metadata", {}).get("mimetype") == mimetype
+
+    storage_file.remove([filename])
