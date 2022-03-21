1817e58f315bf6e6977dc901bed230e8aedefb1b
cloudguruab <ap.brown011@gmail.com>
2022-03-20 23:48:35 -0400

    dev: linted scripts and sorted imports

--- examples/FastAPI/config.py
+++ examples/FastAPI/config.py
@@ -1,15 +1,17 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 class Config:
     """
     Root level configuration for project
     """
-    
-    URL = os.getenv('URL')
-    
-    KEY = os.getenv('KEY')
-    
-    REDIS_URL = os.getenv('LOCAL_REDIS_INSTANCE')
+
+    URL = os.getenv("URL")
+
+    KEY = os.getenv("KEY")
+
+    REDIS_URL = os.getenv("LOCAL_REDIS_INSTANCE")
