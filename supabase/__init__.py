8539a4eeb6109712a600e92736fa5a0a3df343c8
Dani Reinón <dani@dribo.es>
2022-05-06 20:48:44 +0200

    fix: export SupabaseStorageClient

--- supabase/__init__.py
+++ supabase/__init__.py
@@ -6,3 +6,4 @@ from .__version__ import __version__
 from .client import Client, create_client
 from .lib.auth_client import SupabaseAuthClient
 from .lib.realtime_client import SupabaseRealtimeClient
+from .lib.storage_client import SupabaseStorageClient
