f194c51132d771f8d0c166935400b4129521a6b9
cadnce <cadnce@users.noreply.github.com>
2022-10-13 00:31:04 +1100

    Replaced makefile with poetry scripts

---
+++ poetry_scripts.py
@@ -0,0 +1,14 @@
+import subprocess
+    
+def run_cmd(cmd):
+    subprocess.call(cmd, shell=True, check=True)
+
+def run_tests():
+    # Install requirements
+    run_cmd("poetry install")
+    
+    # Run pre-commit tests
+    run_cmd("poetry run pre-commit run --all-files")
+
+    # Generate coverage report
+    run_cmd("poetry run pytest --cov=./ --cov-report=xml --cov-report=html -vv")
