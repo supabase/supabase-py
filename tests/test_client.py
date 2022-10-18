7f6ff50ff1a4b9fe42e5e30c4ef4fc22ac401f6a
Rawand Ahmed Shaswar <rawandahmad698@gmail.com>
2022-10-18 20:59:32 +0300

    Fix test_client.py grammar.

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,12 +6,12 @@ import pytest
 
 
 @pytest.mark.xfail(
-    reason="None of these values should be able to instanciate a client object"
+    reason="None of these values should be able to instantiate a client object"
 )
 @pytest.mark.parametrize("url", ["", None, "valeefgpoqwjgpj", 139, -1, {}, []])
 @pytest.mark.parametrize("key", ["", None, "valeefgpoqwjgpj", 139, -1, {}, []])
-def test_incorrect_values_dont_instanciate_client(url: Any, key: Any) -> None:
-    """Ensure we can't instanciate client with nonsense values."""
+def test_incorrect_values_dont_instantiate_client(url: Any, key: Any) -> None:
+    """Ensure we can't instantiate client with invalid values."""
     from supabase import Client, create_client
 
     _: Client = create_client(url, key)
