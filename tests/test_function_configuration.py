84b7a7164b7ed837a0129ac0a31eda399f9b0bea
Rawand Ahmed Shaswar <rawandahmad698@gmail.com>
2022-10-21 16:25:47 +0300

    Update test_function_configuration.py

--- tests/test_function_configuration.py
+++ tests/test_function_configuration.py
@@ -6,10 +6,10 @@ def test_functions_client_initialization() -> None:
     url = f"https://{ref}.supabase.co"
     # Sample JWT Key
     key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
-    sp = supabase.SupabaseClient(url, key)
+    sp = supabase.Client(url, "testkey")
     sp.functions()
     assert sp.functions_url == f"https://{ref}.functions.supabase.co"
 
     url = "https://localhost:54322"
-    sp_local = supabase.SupabaseClient(url, key)
+    sp_local = supabase.Client(url, "testkey")
     assert sp_local.functions_url == f"{url}/functions/v1"
