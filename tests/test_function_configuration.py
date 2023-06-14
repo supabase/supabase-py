9a9166788792b683f43ea3336c7d1c803d7cf8c4
anand2312 <40204976+anand2312@users.noreply.github.com>
2023-06-14 22:21:47 +0530

    fix: update tests

--- tests/test_function_configuration.py
+++ tests/test_function_configuration.py
@@ -8,7 +8,7 @@ def test_functions_client_initialization() -> None:
     key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
     sp = supabase.Client(url, key)
     sp.functions()
-    assert sp.functions_url == f"https://{ref}.functions.supabase.co"
+    assert sp.functions_url == f"https://{ref}.supabase.co/functions/v1"
 
     url = "https://localhost:54322"
     sp_local = supabase.Client(url, key)
