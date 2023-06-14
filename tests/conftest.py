426e41cffc9d54faf3afc9feca797897cd6b17d5
DanhDevelop <danhdevelop@gmail.com>
2023-06-14 22:44:30 +0700

    add more unittest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,19 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-from supabase import Client, create_client
+from supabase import (
+    create_client,
+    Client,  
+    SupabaseAuthClient,
+    SupabaseRealtimeClient,
+    SupabaseStorageClient,
+
+)
+
+from postgrest import (
+    SyncPostgrestClient,
+    AsyncPostgrestClient,
+)
 
 
 def pytest_configure(config) -> None:
@@ -19,3 +31,19 @@ def supabase() -> Client:
     key = os.environ.get("SUPABASE_TEST_KEY")
     assert key is not None, "Must provide SUPABASE_TEST_KEY environment variable"
     return create_client(url, key)
+
+@pytest.fixture(scope="session")
+def gotrue(supabase: Client) -> SupabaseAuthClient:
+    return supabase.auth
+
+@pytest.fixture(scope="session")
+def postgrest(supabase: Client) -> SyncPostgrestClient:
+    return supabase.postgrest
+
+@pytest.fixture(scope="session")
+def realtime(supabase: Client) -> SupabaseRealtimeClient:
+    return supabase.realtime
+
+@pytest.fixture(scope="session")
+def storage(supabase: Client) -> SupabaseStorageClient:
+    return supabase.storage
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,25 @@
 from __future__ import annotations
 
 from typing import Any
+from faker import Faker
 
 import pytest
+from supabase import Client, create_client, SupabaseAuthClient
+from gotrue import (
+    AuthResponse, 
+    SignUpWithEmailAndPasswordCredentials, 
+    SignInWithEmailAndPasswordCredentials
+)
+from postgrest import (
+    SyncPostgrestClient,
+    AsyncPostgrestClient,
+    APIResponse,
+    APIError,
+)
 
+EMAIL='use.to.test.email@gmail.com'
+PASSWORD='abc123ABC'
+FAKER = Faker()
 
 @pytest.mark.xfail(
     reason="None of these values should be able to instantiate a client object"
@@ -12,6 +28,26 @@ import pytest
 @pytest.mark.parametrize("key", ["", None, "valeefgpoqwjgpj", 139, -1, {}, []])
 def test_incorrect_values_dont_instantiate_client(url: Any, key: Any) -> None:
     """Ensure we can't instantiate client with invalid values."""
-    from supabase import Client, create_client
-
     _: Client = create_client(url, key)
+
+# def test_gotrue_signup_email_password(gotrue: SupabaseAuthClient):
+#     credentials = SignUpWithEmailAndPasswordCredentials(
+#         email = FAKER.email(),
+#         password = FAKER.password()
+#     )
+#     response: AuthResponse = gotrue.sign_up(credentials=credentials)
+#     assert response.user is not None, "Failed to sign up user with email & password!"
+
+def test_gotrue_signin_email_password(gotrue: SupabaseAuthClient):
+    credentials = SignInWithEmailAndPasswordCredentials(
+        email=EMAIL,
+        password=PASSWORD
+    )
+    response: AuthResponse = gotrue.sign_in_with_password(credentials=credentials)
+    assert response.user is not None, "Failed to sign in user with email & password!"
+
+@pytest.mark.xfail(raises=APIError)
+def test_postgrest_select_table(gotrue: SupabaseAuthClient, postgrest: SyncPostgrestClient):
+    response: APIResponse = postgrest.table("testing").select("*").execute()
+    assert response.data is not None, "Failed to select value from table!"
+    
