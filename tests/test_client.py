64134ef84cb8b2f4277f42278c94edda9f32b819
DanhDevelop <danhdevelop@gmail.com>
2023-06-17 10:14:06 +0700

    complete unittest for postgrest to select table with/without RLS

--- tests/test_client.py
+++ tests/test_client.py
@@ -30,13 +30,13 @@ def test_incorrect_values_dont_instantiate_client(url: Any, key: Any) -> None:
     """Ensure we can't instantiate client with invalid values."""
     _: Client = create_client(url, key)
 
-# def test_gotrue_signup_email_password(gotrue: SupabaseAuthClient):
-#     credentials = SignUpWithEmailAndPasswordCredentials(
-#         email = FAKER.email(),
-#         password = FAKER.password()
-#     )
-#     response: AuthResponse = gotrue.sign_up(credentials=credentials)
-#     assert response.user is not None, "Failed to sign up user with email & password!"
+def test_gotrue_signup_email_password(gotrue: SupabaseAuthClient):
+    credentials = SignUpWithEmailAndPasswordCredentials(
+        email = FAKER.email(),
+        password = FAKER.password()
+    )
+    response: AuthResponse = gotrue.sign_up(credentials=credentials)
+    assert response.user is not None, "Failed to sign up user with email & password!"
 
 def test_gotrue_signin_email_password(gotrue: SupabaseAuthClient):
     credentials = SignInWithEmailAndPasswordCredentials(
@@ -46,8 +46,15 @@ def test_gotrue_signin_email_password(gotrue: SupabaseAuthClient):
     response: AuthResponse = gotrue.sign_in_with_password(credentials=credentials)
     assert response.user is not None, "Failed to sign in user with email & password!"
 
-@pytest.mark.xfail(raises=APIError)
-def test_postgrest_select_table(gotrue: SupabaseAuthClient, postgrest: SyncPostgrestClient):
+# Turn off Row Level Security to pass this test.
+def test_postgrest_RLS_OFF_select_table(postgrest: SyncPostgrestClient):
     response: APIResponse = postgrest.table("testing").select("*").execute()
-    assert response.data is not None, "Failed to select value from table!"
-    
+    assert len(response.data) > 0, "Failed to select value from table with RLS OFF!"
+
+# Row Level Security : remember to add policy to allow authenticated user to read the table.
+# Follow guide line: https://supabase.com/docs/learn/auth-deep-dive/auth-row-level-security
+def test_postgrest_RLS_ON_select_table(gotrue: SupabaseAuthClient, postgrest: SyncPostgrestClient):
+    # Manually pass access token to postgrest to fullfill auth parameters 
+    postgrest.auth(gotrue.get_session().access_token)
+    response: APIResponse = postgrest.table("testing").select("*").execute()
+    assert len(response.data) > 0, "Failed to select value from table with RLS ON!"
