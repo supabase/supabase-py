5f052ee0d064e67b68f27fc77a08a0c6d4fc2257
joel@joellee.org <joel@joellee.org>
2023-10-03 22:21:47 +0800

    test: remove call to functions

--- tests/test_function_configuration.py
+++ tests/test_function_configuration.py
@@ -7,7 +7,6 @@ def test_functions_client_initialization() -> None:
     # Sample JWT Key
     key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
     sp = supabase.Client(url, key)
-    sp.functions()
     assert sp.functions_url == f"https://{ref}.supabase.co/functions/v1"
 
     url = "https://localhost:54322"
