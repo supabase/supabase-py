f4fd3f6edfc641bd02bc8dd3e7a26bb1d0f32a4d
Andrew Smith <a.smith@silentworks.co.uk>
2024-02-14 19:55:29 +0000

    fix: add missing ClientOptions to the main init file

--- supabase/__init__.py
+++ supabase/__init__.py
@@ -4,6 +4,7 @@ from storage3.utils import StorageException
 
 from .__version__ import __version__
 from ._sync.auth_client import SyncSupabaseAuthClient as SupabaseAuthClient
+from ._sync.client import ClientOptions
 from ._sync.client import SyncClient as Client
 from ._sync.client import SyncStorageClient as SupabaseStorageClient
 from ._sync.client import create_client
