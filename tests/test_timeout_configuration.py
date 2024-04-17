b3bcb65bf105056207bfe4b26f998dc4b225cf04
Juan Carlos <juancarlospaco@gmail.com>
2024-04-17 14:29:27 -0300

    Fix Timeout for long running tasks

---
+++ tests/test_timeout_configuration.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+
+def test_new_timeout_configuration():
+    from supabase import create_client, Client
+    from supabase.lib.client_options import ClientOptions
+    import os
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+
+    client = create_client(url, key)
+    data   = [{"item": value} for value in range(0,1000000)]
+
+    client.table("sample").insert(data).execute()
+
+
+@pytest.mark.xfail(
+    reason="postgrest client timeout reached before processing the request."
+)
+def test_passing_wrong_timeout_value():
+    from supabase import create_client, Client
+    from supabase.lib.client_options import ClientOptions
+    import os
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+
+    options = ClientOptions(postgrest_client_timeout = 10)
+    client  = create_client(url, key, options)
+    data    = [{"item": value} for value in range(0,1000000)]
+
+    client.table("sample").insert(data).execute()
