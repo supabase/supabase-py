ad2fe1cbe86b5645fb88ff6c9d3ea9eb0c281ed3
Daniil <48557356+fakelog@users.noreply.github.com>
2024-04-17 20:23:39 +0500

    chore: add import async client (#760)

--- supabase/__init__.py
+++ supabase/__init__.py
@@ -2,15 +2,31 @@ from postgrest import APIError as PostgrestAPIError
 from postgrest import APIResponse as PostgrestAPIResponse
 from storage3.utils import StorageException
 
+# Version
 from .__version__ import __version__
+
+# Async Client
+from ._async.auth_client import AsyncSupabaseAuthClient as ASupabaseAuthClient
+from ._async.client import AsyncClient as AClient
+from ._async.client import AsyncStorageClient as ASupabaseStorageClient
+from ._async.client import ClientOptions as AClientOptions
+from ._async.client import create_client as acreate_client
+
+# Sync Client
 from ._sync.auth_client import SyncSupabaseAuthClient as SupabaseAuthClient
 from ._sync.client import ClientOptions
 from ._sync.client import SyncClient as Client
 from ._sync.client import SyncStorageClient as SupabaseStorageClient
 from ._sync.client import create_client
+
+# Realtime Client
 from .lib.realtime_client import SupabaseRealtimeClient
 
 __all__ = [
+    "acreate_client",
+    "AClient",
+    "ASupabaseAuthClient",
+    "ASupabaseStorageClient",
     "create_client",
     "Client",
     "SupabaseAuthClient",
