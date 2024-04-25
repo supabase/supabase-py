580d093ec7c41b7735889c4499d801aea5a54a14
Juan Carlos <juancarlospaco@gmail.com>
2024-04-24 23:03:37 -0300

    fixes

--- tests/test_timeout_configuration.py
+++ tests/test_timeout_configuration.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
-from typing import Any
-
 import pytest
 
 
 def test_new_timeout_configuration():
-    from supabase import create_client, Client
-    from supabase.lib.client_options import ClientOptions
     import os
+
+    from supabase import create_client
 
     url: str = os.environ.get("SUPABASE_TEST_URL")
     key: str = os.environ.get("SUPABASE_TEST_KEY")
 
     client = create_client(url, key)  # No options used here.
-    data   = [{"item": value} for value in range(0, 1000000)]
+    data = [{"item": value} for value in range(0, 1000000)]
 
     client.table("sample").insert(data).execute()
 
@@ -23,15 +21,16 @@ def test_new_timeout_configuration():
     reason="postgrest client timeout reached before processing the request."
 )
 def test_passing_wrong_timeout_value():
-    from supabase import create_client, Client
-    from supabase.lib.client_options import ClientOptions
     import os
+
+    from supabase import create_client
+    from supabase.lib.client_options import ClientOptions
 
     url: str = os.environ.get("SUPABASE_TEST_URL")
     key: str = os.environ.get("SUPABASE_TEST_KEY")
 
-    options = ClientOptions(postgrest_client_timeout = 5)  # Explicit timeout.
-    client  = create_client(url, key, options)
-    data    = [{"item": value} for value in range(0, 1000000)]
+    options = ClientOptions(postgrest_client_timeout=5)  # Explicit timeout.
+    client = create_client(url, key, options)
+    data = [{"item": value} for value in range(0, 1000000)]
 
     client.table("sample").insert(data).execute()
