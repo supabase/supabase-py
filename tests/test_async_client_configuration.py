1ad6a914e11a6d02d701e347e353687bf56a7b25
Juan Carlos <juancarlospaco@gmail.com>
2024-04-29 22:11:34 -0300

    update test

--- tests/test_async_client_configuration.py
+++ tests/test_async_client_configuration.py
@@ -1,20 +1,30 @@
-from __future__ import annotations
-
-from typing import Any
+import asyncio
+import os
 
 import pytest
 
+from supabase._async.client import create_client
 
-def test_async_configuration():
-    import httpcore
-    import asyncio
-    import os
-    from supabase._async.client import create_client
+pytest_plugins = ("pytest_asyncio", )
 
+
+@pytest.mark.asyncio
+async def test_async_configuration():
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
 
-    client = asyncio.run(create_client(url, key))
-    data   = [{"item": value} for value in range(0, 100000)]
+    """
+    Initializing an AsynClient:
+    Using asyncio.create_task instead of asyncio.run allows you to reuse
+    the same client across different request, avoiding an event loop error.
+    """
 
-    asyncio.run(client.table("sample").insert(data).execute())
+    client = await asyncio.create_task(create_client(url, key))
+    data = [{"item": value} for value in range(0, 100000)]
+
+    # insert
+    await client.table("sample").insert(data).execute()
+    # select
+    await client.table("sample").select("*").execute()
+    # delete
+    await client.table("sample").delete().gt("item", 100).execute()
