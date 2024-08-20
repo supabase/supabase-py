add4eddd59106b3bacbaeffbec0e3b096c7b2857
Andrew Smith <a.smith@silentworks.co.uk>
2024-08-20 23:17:45 +0000

    feat: add third-party auth support

---
+++ supabase/utils.py
@@ -0,0 +1,16 @@
+from gotrue.errors import AuthError
+
+
+# Create an exception class when user does not provide a valid url or key.
+class SupabaseException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class AuthProxy:
+    def __getattr__(self, attr):
+        raise AuthError(
+            f"Supabase Client is configured with the access_token option, accessing supabase.auth.{attr} is not possible.",
+            None,
+        )
