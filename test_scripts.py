c064ea6691f680aca54de9101f311ead9c72f99b
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-07-30 10:55:06 -0300

    feat: use uv instead of poetry

---
+++ test_scripts.py
@@ -0,0 +1,12 @@
+from subprocess import Popen
+
+
+def run_cmd(cmd: str) -> Popen:
+    return Popen(cmd.split())
+
+def run_tests():
+    # Install requirements
+    with run_cmd('uv run pre-commit run --all-files') as precommit, \
+         run_cmd('uv run pytest --cov=./ --cov-report=xml --cov-report=html -vv') as pytests:
+        pass
+    return precommit.returncode and pytests.returncode
