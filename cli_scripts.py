458bb7fd71b4ba110b422d4dc7787c3ac8c65f90
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-07-30 11:46:22 -0300

    fix: split `dev`, include dev as default dependency-group

--- cli_scripts.py
+++ cli_scripts.py
@@ -1,17 +1,20 @@
 from subprocess import Popen
 
 
-def run_cmd(cmd: str) -> Popen:
+def spawn(cmd: str) -> Popen:
     return Popen(cmd.split())
+
+
+def spawn_precommit() -> Popen:
+    return spawn("uv run pre-commit run --all-files")
+
+
+def spawn_pytest() -> Popen:
+    return spawn("uv run pytest --cov=./ --cov-report=xml --cov-report=html -vv")
 
 
 def run_tests():
     # Install requirements
-    with (
-        run_cmd("uv run pre-commit run --all-files") as precommit,
-        run_cmd(
-            "uv run pytest --cov=./ --cov-report=xml --cov-report=html -vv"
-        ) as pytests,
-    ):
-        pass
+    with spawn_precommit() as precommit, spawn_pytest() as pytests:
+        pass  # implicitly wait for all of them to return
     return precommit.returncode and pytests.returncode
