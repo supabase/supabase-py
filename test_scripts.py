b755bca381efc878764f6023dad60dbef59c85a3
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-07-30 10:59:22 -0300

    fix: accept ruff formatting

--- test_scripts.py
+++ test_scripts.py
@@ -4,9 +4,14 @@ from subprocess import Popen
 def run_cmd(cmd: str) -> Popen:
     return Popen(cmd.split())
 
+
 def run_tests():
     # Install requirements
-    with run_cmd('uv run pre-commit run --all-files') as precommit, \
-         run_cmd('uv run pytest --cov=./ --cov-report=xml --cov-report=html -vv') as pytests:
+    with (
+        run_cmd("uv run pre-commit run --all-files") as precommit,
+        run_cmd(
+            "uv run pytest --cov=./ --cov-report=xml --cov-report=html -vv"
+        ) as pytests,
+    ):
         pass
     return precommit.returncode and pytests.returncode
