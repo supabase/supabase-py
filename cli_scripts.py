a458b02289e13415d71c5b5787964a8c04a867e8
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-07-31 10:55:20 -0300

    fix: run uv format

--- cli_scripts.py
+++ cli_scripts.py
@@ -20,20 +20,25 @@ def unasync():
 def sed(before: str, after: str):
     return subprocess.run(["sed", "-i", "", before, after], check=True)
 
+
 def build_sync():
     substs = [
-        ('s/asyncio.create_task(self.realtime.set_auth(access_token))//g', "supabase/_sync/client.py"),
-        ('s/asynch/synch/g', "supabase/_sync/auth_client.py"),
-        ('s/Async/Sync/g', "supabase/_sync/auth_client.py"),
-        ('s/Async/Sync/g', "supabase/_sync/client.py"),
-        ('s/create_async_client/create_client/g', "tests/_sync/test_client.py"),
-        ('s/SyncClient/Client/gi', "tests/_sync/test_client.py"),
-        ('s/SyncHTTPTransport/HTTPTransport/g', "tests/_sync/test_client.py"),
-        ('s/SyncMock/Mock/g', "tests/_sync/test_client.py"),
+        (
+            "s/asyncio.create_task(self.realtime.set_auth(access_token))//g",
+            "supabase/_sync/client.py",
+        ),
+        ("s/asynch/synch/g", "supabase/_sync/auth_client.py"),
+        ("s/Async/Sync/g", "supabase/_sync/auth_client.py"),
+        ("s/Async/Sync/g", "supabase/_sync/client.py"),
+        ("s/create_async_client/create_client/g", "tests/_sync/test_client.py"),
+        ("s/SyncClient/Client/gi", "tests/_sync/test_client.py"),
+        ("s/SyncHTTPTransport/HTTPTransport/g", "tests/_sync/test_client.py"),
+        ("s/SyncMock/Mock/g", "tests/_sync/test_client.py"),
     ]
     unasync()
-    for (left, right) in substs:
+    for left, right in substs:
         sed(left, right)
+
 
 def run_tests():
     precommit()
