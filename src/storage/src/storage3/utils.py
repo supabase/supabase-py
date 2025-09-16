119bb3d865ac9c4a29bcfa0526fa7c2376555a2d
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-09-16 16:28:10 -0300

    fix(storage): bump breaking version requirements to 3.0.0
    otherwise bumping its version to 2.19 would be a breaking change, as
    this deprecated method would raise an error

--- src/storage/src/storage3/utils.py
+++ src/storage/src/storage3/utils.py
@@ -7,14 +7,14 @@ from .version import __version__
 
 class SyncClient(Client):
     @deprecated(
-        "0.11.3", "1.0.0", __version__, "Use `Client` from the httpx package instead"
+        "0.11.3", "3.0.0", __version__, "Use `Client` from the httpx package instead"
     )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @deprecated(
         "0.11.3",
-        "1.0.0",
+        "3.0.0",
         __version__,
         "Use `close` method from `Client` in the httpx package instead",
     )
