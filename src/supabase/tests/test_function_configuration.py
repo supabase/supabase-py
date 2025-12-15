78ebf2c62107bd2cfc7e2ee4bcdd3388f091a6e3
Leonardo Santiago <leonardo.ribeiro.santiago@gmail.com>
2025-12-15 10:38:21 -0300

    feat(supabase): use yarl URL builder in supabase as well (#1331)

--- src/supabase/tests/test_function_configuration.py
+++ src/supabase/tests/test_function_configuration.py
@@ -7,8 +7,8 @@ def test_functions_client_initialization() -> None:
     # Sample JWT Key
     key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
     sp = supabase.Client(url, key)
-    assert sp.functions_url == f"https://{ref}.supabase.co/functions/v1"
+    assert str(sp.functions_url) == f"https://{ref}.supabase.co/functions/v1"
 
     url = "https://localhost:54322"
     sp_local = supabase.Client(url, key)
-    assert sp_local.functions_url == f"{url}/functions/v1"
+    assert str(sp_local.functions_url) == f"{url}/functions/v1"
