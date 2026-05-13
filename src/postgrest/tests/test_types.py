e7449eb5581894410782ff0926536889b454dcc6
snoopuppy582 <mnb0968@naver.com>
2026-05-13 22:12:16 +0900

    test: compare JSON adapter schema

--- src/postgrest/tests/test_types.py
+++ src/postgrest/tests/test_types.py
@@ -1,10 +1,11 @@
+from pydantic import TypeAdapter
+from pydantic.types import JsonValue
+
 from postgrest.types import JSONAdapter
 
 
-def test_json_adapter_uses_pydantic_json_value_schema():
-    assert JSONAdapter.core_schema["schema"]["schema_ref"].startswith(
-        "pydantic.types.JsonValue:"
-    )
+def test_json_adapter_schema_matches_pydantic_json_value():
+    assert JSONAdapter.core_schema == TypeAdapter(JsonValue).core_schema
 
 
 def test_json_adapter_validates_nested_json_bytes():
